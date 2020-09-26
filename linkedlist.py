d7d77113e78e8b80a2e1a12f091f82179972cd2b
Muhamad Nahrowi <32912114+projectnahrowi@users.noreply.github.com>
2020-09-26 17:11:25 +0700

    Add files via upload

---
+++ linkedlist.py
@@ -0,0 +1,28 @@
+# Muhamad Nahrowi
+# Single linklist
+# 26 september 2020
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
+        
+
+
+class Linked_list:
+    def __init__(self):
+        self.head = None
+        
+    def print_LL(self):
+        if self.head is None:
+            print("inked list is empty!")
+        else:
+            n= self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
+                
+LL1 = Linked_list()
+LL1.print_LL()
+        
+        
